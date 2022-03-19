acbc50f67f45b6324ee992da46b633adb19e020c
LeeHemingway <coherence0815@gmail.com>
2022-03-19 17:49:48 +0900

    add handlebar example

---
+++ express/templateEngines/handlebars/index.js
@@ -0,0 +1,21 @@
+const { engine } = require('express-handlebars')
+const express = require('express')
+const app = express()
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views')
+
+app.get('/', function(req, res){
+  const data = {
+    hello: 1,
+    world: 2
+  } 
+  res.render('aaa', data)
+})
+
+app.listen(3000, () => {
+  console.log('server is running and listen to port 3000')
+})
+
+
