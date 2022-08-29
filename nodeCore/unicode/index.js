e3f8f95d79c35d8635eb70cb6bafbb3c442ed3d5
LeeHemingway <coherence0815@gmail.com>
2022-08-29 15:26:07 +0900

    add some checks

--- nodeCore/unicode/index.js
+++ nodeCore/unicode/index.js
@@ -1,9 +1,50 @@
+const punycode = require('punycode');
+
+var regexSymbolWithCombiningMarks = /(\P{Mark})(\p{Mark}+)/gu;
+
+function countSymbolsIgnoringCombiningMarks(string) {
+	var stripped = string.replace(regexSymbolWithCombiningMarks, function($0, symbol, combiningMarks) {
+		return symbol;
+	});
+	
+  return punycode.ucs2.decode(stripped).length;
+}
+
+function countSymbolsPedantically(theStr) {
+	var normalized = theStr.normalize('NFC');
+	return punycode.ucs2.decode(normalized).length;
+}
+
+function countSymbols2(theStr) {
+	return punycode.ucs2.decode(theStr).length;
+}
+
+function countSymbols(theStr) {
+	return Array.from(theStr).length;
+}
+
 
 const str = 'a禰󠄀豆子に襲わ';
 console.log(str)
 console.log(`length: ${str.length}`);
+console.log(`count 1: ${countSymbols(str)}`)
+console.log(`count 2: ${countSymbols2(str)}`)
+console.log(`count 3: ${countSymbolsPedantically(str)}`)
+console.log(`count 4: ${countSymbolsIgnoringCombiningMarks(str)}`)
+
+// codePointAt shows the whole number
+// charCodeAt shows only 0 to 65535, 
+//  and it show the first part if number greater than 65535
 for (var i=0; i<str.length; ++i) {
-  console.log(`${str[i]}, ${str.codePointAt(i)}, ${str.charCodeAt(i)}`);
+  console.log({
+    "char": str[i], 
+    "point": str.codePointAt(i), 
+    "code":str.charCodeAt(i)
+  });
+}
+
+for (const symbol of str) {
+	console.log(symbol)
 }
 
 
@@ -16,3 +57,6 @@ console.log(utf8Encode.encode("に"));
 console.log(utf8Encode.encode("襲"));
 console.log(utf8Encode.encode("わ"));
 
+
+const uint8array = new TextEncoder().encode("a禰󠄀豆子に襲わ");
+console.log(uint8array)
